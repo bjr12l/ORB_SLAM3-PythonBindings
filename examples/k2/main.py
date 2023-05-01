439fd26896201489db4105cbad05fba4ffb60e0b
Vlad Bondarenko <vladislavbond1998@gmail.com>
2023-05-01 13:22:00 +0000

    refactor: move all the sensor utils into one file

--- examples/k2/main.py
+++ examples/k2/main.py
@@ -8,26 +8,18 @@ from tqdm import tqdm
 PROJECT_DIR = Path("..") / ".."
 VOCABLUARY_PATH = PROJECT_DIR / "Vocabluary" / "ORBvoc.txt"
 
-VIDEO_FOLDER = Path("gopro_vid")
+VIDEO_FOLDER = Path("cam1_lens100_alt600")
 CONFIG_PATH = VIDEO_FOLDER / "config.yaml"
 ORB_CONFIG_PATH = VIDEO_FOLDER / "orb_config.yaml"
 
 DATA_DIR = PROJECT_DIR / "data" / "k2" / VIDEO_FOLDER
 
 from orbslam_k2 import ORBSLAM3
-from sensor_sync import sync_logs, sync_video
+from sensor_utils import sync_video, read_synced_logs, convert_logs_to_csv
 
 def timedelta_from_string(timestamp_str: str) -> pd.Timedelta:
-    minutes, seconds = timestamp_str.split(":")
-    return pd.Timedelta(minutes=int(minutes), seconds=int(seconds))
-
-def read_synced_logs(path_to_logs: Path, fps, log_start):
-    logs = pd.read_csv(path_to_logs).sort_values(by="microseconds_from_start")
-    logs["Alt"] -= (logs["Alt"].min() - 1)
-    logs = sync_logs(logs, fps, log_start)
-    logs["seconds_from_start"] = logs.index.total_seconds()
-    return logs
-
+    minutes, seconds, milliseconds = timestamp_str.split(":")
+    return pd.Timedelta(minutes=int(minutes), seconds=int(seconds), milliseconds=int(milliseconds))
 
 if __name__ == "__main__":
     with open(str(CONFIG_PATH), "r") as f:
@@ -37,15 +29,16 @@ if __name__ == "__main__":
     log_start = timedelta_from_string(config["log_start"]) + offset
     video_start = timedelta_from_string(config["video_start"]) + offset
 
+    print(f"Starting video from {video_start}")
+
     vid = cv2.VideoCapture(str(DATA_DIR / config["video_name"]))
     vid, fps = sync_video(vid, video_start)
 
-    logs = read_synced_logs(DATA_DIR / config["log_name"], fps, log_start)
+    logs = read_synced_logs(DATA_DIR / config["csv_log_name"], fps, log_start)
 
-    slam = ORBSLAM3(str(VOCABLUARY_PATH), str(ORB_CONFIG_PATH), orbslam3.Sensor.MONOCULAR, False)
+    slam = ORBSLAM3(str(VOCABLUARY_PATH), str(ORB_CONFIG_PATH), orbslam3.Sensor.MONOCULAR, True)
 
     for i in tqdm(range(5000)):
         _, frame = vid.read()
         log = logs.iloc[i]
-        small_frame = cv2.resize(frame, slam.new_frame_size)
         slam.step(frame, log)
---
+++ examples/k2/sensor_utils.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import cv2
+import os
+from pymap3d import geodetic2ned
+from typing import Tuple, List
+from pathlib import Path
+from functools import reduce
+from pymavlink import mavutil
+
+
+SENSORS_COLUMNS = ['GyrX', 'GyrY', 'GyrZ', 'AccX', 'AccY', 'AccZ']
+ANGLE_COLUMNS = ['DesRoll', 'Roll', 'DesPitch', 'Pitch', 'DesYaw', 'Yaw']
+NAVIGATION_COLUMNS = ['Lat', 'Lng', 'Alt']
+
+
+def convert_logs_to_csv(logs_path: str, output_path: str) -> pd.DataFrame:
+    if os.path.exists(output_path):
+        return pd.read_csv(output_path).set_index("microseconds_from_start")
+    columns_manager = {
+        'IMU': SENSORS_COLUMNS,
+        'ATT': ANGLE_COLUMNS,
+        'GPS': NAVIGATION_COLUMNS
+    }
+
+    allowed_messages = list(columns_manager.keys())
+    all_columns: List[str] = reduce(lambda x, y: x + y, list(columns_manager.values()))
+
+    reader = mavutil.mavlink_connection(logs_path)
+
+    data = {}
+    types = []
+    while True:
+        message = reader.recv_match()
+        if message is None:
+            break
+
+        message_type = message.fmt.name
+        types.append(message_type)
+        if message_type in allowed_messages:
+            message_data = message.to_dict()
+            timestamp = message_data['TimeUS']
+            if timestamp not in data:
+                data[timestamp] = {column: None for column in all_columns}
+
+            for column in columns_manager[message_type]:
+                data[timestamp][column] = message_data[column]
+
+    df = pd.DataFrame(data).T
+    df = df.fillna(method='ffill')
+    if output_path:
+        df.to_csv(output_path, index_label='microseconds_from_start')
+    return df
+
+def read_synced_logs(path_to_logs: Path, fps, log_start):
+    logs = pd.read_csv(path_to_logs).sort_values(by="microseconds_from_start")
+    logs["Alt"] -= (logs["Alt"].min() - 1)
+    logs = sync_logs(logs, fps, log_start)
+    logs["seconds_from_start"] = logs.index.total_seconds()
+    return logs
+
+def sync_logs(flight_log: pd.DataFrame, fps: int, log_offset: pd.Timedelta) -> pd.DataFrame:
+    flight_log.index = pd.TimedeltaIndex(pd.to_timedelta(flight_log["microseconds_from_start"], unit="us"))
+    flight_log = flight_log.loc[log_offset:].resample(f"{(1 / fps):.3f}S").mean()
+    start_lat, start_lng, start_alt = flight_log.iloc[0][["Lat", "Lng", "Alt"]]
+    flight_log[["x", "y", "z"]] = flight_log.apply(
+        lambda row: geodetic2ned(row["Lat"], row["Lng"], row["Alt"], start_lat, start_lng, start_alt),
+        axis=1
+    ).tolist()
+    flight_log["z"] = -flight_log["z"]
+    return flight_log
+
+def sync_video(vid: cv2.VideoCapture, ofset: pd.Timedelta) -> Tuple[cv2.VideoCapture, int]:
+    fps = vid.get(cv2.CAP_PROP_FPS)
+    vid.set(cv2.CAP_PROP_POS_FRAMES, int(fps * ofset.seconds))
+    return vid, fps
